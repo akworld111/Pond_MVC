68b25db8264b2149fd10aee1980c63fb451c1e6e
AK <akhilawin009@gmail.com>
2020-06-24 15:31:50 +0800

    initial

---
+++ views/templates/heads.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pond Home</title>
+    <link href="<?= server_path('assets/styles/bs.main_styles.css'); ?>" rel="stylesheet">
+    <link href="<?= server_path('assets/styles/index.css'); ?>" rel="stylesheet" type="text/css" />
+</head>
